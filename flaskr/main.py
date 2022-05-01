299855441adfa854044d8f8e3c9a2306efa93211
hidekis1126 <consulart.hmk@gmail.com>
2022-05-02 03:22:35 +0900

    flaskでアプリ作成

---
+++ flaskr/main.py
@@ -0,0 +1,46 @@
+from django.shortcuts import redirect
+from flaskr import app
+from flask import render_template, request, redirect, url_for
+import sqlite3
+
+from flaskr.db import DATABASE
+DATABASE = 'database.db'
+
+# 起動について：flaskrと同じ階層から一時的な環境変数取得、flask実行
+# flask-tutorial %export FLASK_APP=flaskr
+# flask-tutorial %export FLASK_ENV=development
+# flask run
+# webサーバー： http://127.0.0.1:5000　
+
+@app.route('/')
+def index():
+    con = sqlite3.connect(DATABASE)
+    db_books = con.execute('SELECT * FROM books').fetchall()
+    con.close()
+
+    books = []
+    for row in db_books:
+        books.append({'title': row[0], 'price': row[1], 'arrival_day': row[2]})
+    return render_template(
+        'index.html',
+        books=books
+    )
+
+@app.route('/form')
+def form():
+    return render_template(
+        'form.html'
+    )
+
+@app.route('/register', methods=['POST'])
+def register():
+    title = request.form['title']
+    price = request.form['price']
+    arrival_day = request.form['arrival_day']
+
+    con = sqlite3.connect(DATABASE)
+    con.execute('INSERT INTO books VALUES(?,?,?)',
+                [title, price, arrival_day])
+    con.commit()
+    con.close()
+    return redirect(url_for('index'))
